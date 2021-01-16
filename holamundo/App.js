b7b21e931cac81c25bc87658df0113750aed1a6e
Eraldo Vicente <eraldovicente2@gmail.com>
2021-01-16 16:58:09 -0200

    SectionList

--- holamundo/App.js
+++ holamundo/App.js
@@ -3,23 +3,48 @@ import {
   StyleSheet,
   View,
   Text,
-  FlatList
+  FlatList,
+  SectionList
 } from 'react-native';
+
+const data = [
+  { key: '1', name: 'Eraldo'},
+  { key: '2', name: 'galinha'},
+  { key: '3', name: 'ganso'},
+  { key: '4', name: 'cachorro'},
+  { key: '5', name: 'pato'},
+]
 
 const App = () => {
 
   return (
       <View style={styles.container}>
-        <FlatList
-          data={[
-            { key: '1', name: 'Eraldo'},
-            { key: '2', name: 'galinha'},
-            { key: '3', name: 'ganso'},
-            { key: '4', name: 'cachorro'},
-            { key: '5', name: 'pato'},
+        <SectionList
+          sections={[
+            {
+              title: 'Group 1', 
+              data: [
+                { key: '1', name: 'Eraldo'},
+                { key: '2', name: 'galinha'},
+                { key: '3', name: 'ganso'},
+                { key: '4', name: 'cachorro'},
+                { key: '5', name: 'pato'},
+              ]
+            },
+            {
+              title: 'Group 2', 
+              data: [
+                { key: '6', name: 'Eraldo'},
+                { key: '7', name: 'galinha'},
+                { key: '8', name: 'ganso'},
+                { key: '9', name: 'cachorro'},
+                { key: '10', name: 'pato'},
+              ]
+            }
           ]}
           renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
-        />
+          renderSectionHeader={({ section }) => <Text style={styles.section}>{section.title}</Text>}
+        />  
       </View>
   );
 };
@@ -38,6 +63,12 @@ const styles = StyleSheet.create({
     height: 50,
     borderBottomColor: '#ccc',
     borderBottomWidth: 1
+  },
+  section: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    backgroundColor: '#EEE',
+    paddingTop: 2
   }
 });
 
