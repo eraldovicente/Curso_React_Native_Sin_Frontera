911cbf228e8c27a500a82ffc15abcba400760390
Eraldo Vicente <eraldovicente2@gmail.com>
2021-01-16 17:49:07 -0200

    Obtener datos de una API

--- holamundo/App.js
+++ holamundo/App.js
@@ -1,55 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   StyleSheet,
   View,
   Text,
-  FlatList,
-  SectionList
+  FlatList
 } from 'react-native';
-
-const data = [
-  { key: '1', name: 'Eraldo'},
-  { key: '2', name: 'galinha'},
-  { key: '3', name: 'ganso'},
-  { key: '4', name: 'cachorro'},
-  { key: '5', name: 'pato'},
-]
 
 const App = () => {
 
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => {
+        setUsers(data)
+        setLoading(false)
+      })
+  }, [])
+
+  if(loading) {
+    return <View style={styles.center}><Text>Cargando...</Text></View>
+  }
+
   return (
       <View style={styles.container}>
-        <SectionList
-          sections={[
-            {
-              title: 'Group 1', 
-              data: [
-                { key: '1', name: 'Eraldo'},
-                { key: '2', name: 'galinha'},
-                { key: '3', name: 'ganso'},
-                { key: '4', name: 'cachorro'},
-                { key: '5', name: 'pato'},
-              ]
-            },
-            {
-              title: 'Group 2', 
-              data: [
-                { key: '6', name: 'Eraldo'},
-                { key: '7', name: 'galinha'},
-                { key: '8', name: 'ganso'},
-                { key: '9', name: 'cachorro'},
-                { key: '10', name: 'pato'},
-              ]
-            }
-          ]}
-          renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
-          renderSectionHeader={({ section }) => <Text style={styles.section}>{section.title}</Text>}
-        />  
+        <FlatList
+          data={users}
+          renderItem={({ item }) => <Text>{item.name}</Text>}
+          keyExtractor={item => String(item.id)}
+        />
       </View>
   );
 };
 
 const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
   container: {
     flex: 1,
     backgroundColor: '#FFF',
@@ -63,12 +54,6 @@ const styles = StyleSheet.create({
     height: 50,
     borderBottomColor: '#ccc',
     borderBottomWidth: 1
-  },
-  section: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    backgroundColor: '#EEE',
-    paddingTop: 2
   }
 });
 
