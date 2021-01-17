2b988bb1a896cd6a21d3853ac13ee113eac5300b
Eraldo Vicente <eraldovicente2@gmail.com>
2021-01-16 22:25:56 -0200

    Alert, crea dialogos nativos

--- holamundo/App.js
+++ holamundo/App.js
@@ -3,9 +3,26 @@ import {
   StyleSheet,
   View,
   Text,
-  Modal,
-  Button
+  Button,
+  Alert
 } from 'react-native';
+
+const crearDialogo = () => Alert.alert(
+  'Titulo', 
+  'Subtitulo o mensaje que podemos agregar a este dialogo...',
+  [
+    {
+      text: 'Cancelar',
+      onPress: () => {},
+      style: 'cancel'
+    },
+    {
+      text: 'Aceptar',
+      onPress: () => console.log('boton presionado')
+    }
+  ],
+  { cancelable: false }
+)
 
 const App = () => {
 
@@ -13,37 +30,12 @@ const App = () => {
 
   return (
       <View style={styles.container}>
-        <Modal
-          animationType="slide"
-          transparent={false}
-          visible={modal}
-        >
-          <View style={styles.center}>
-            <View style={styles.content}>
-              <Text>Soy un modal</Text>
-              <Button title="Cerrar modal" onPress={() => setModal(!modal)}/>
-            </View>
-          </View>
-        </Modal>
-        <Button title="Abrir modal" onPress={() => setModal(!modal)} />
+        <Button title='Abrir dialogo' onPress={crearDialogo} />
       </View>
   );
 };
 
 const styles = StyleSheet.create({
-  content: {
-    backgroundColor: '#FFF',
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    margin: 25
-  },
-  center: {
-    flex: 1,
-    alignItems: 'stretch',
-    justifyContent: 'center',
-    backgroundColor: 'rgba(0,0,0,0.3)'
-  },
   container: {
     flex: 1,
     backgroundColor: '#FFF',
