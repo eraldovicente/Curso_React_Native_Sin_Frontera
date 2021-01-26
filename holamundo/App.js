da51bbfd383a43a24ed6de566ad6dd2a6890fcf1
Eraldo Vicente <eraldovicente2@gmail.com>
2021-01-25 23:16:51 -0200

    Geolocalizacion

--- holamundo/App.js
+++ holamundo/App.js
@@ -1,36 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import {
   StyleSheet,
   View,
-  Text,
-  Button,
-  Alert
+  Text
 } from 'react-native';
-
-const crearDialogo = () => Alert.alert(
-  'Titulo', 
-  'Subtitulo o mensaje que podemos agregar a este dialogo...',
-  [
-    {
-      text: 'Cancelar',
-      onPress: () => {},
-      style: 'cancel'
-    },
-    {
-      text: 'Aceptar',
-      onPress: () => console.log('boton presionado')
-    }
-  ],
-  { cancelable: false }
-)
 
 const App = () => {
 
-  const [modal, setModal] = useState(false)
-
   return (
       <View style={styles.container}>
-        <Button title='Abrir dialogo' onPress={crearDialogo} />
       </View>
   );
 };
